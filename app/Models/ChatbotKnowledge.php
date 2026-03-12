391ad4afd78d6bd6d4ece0c89d7b5a7d761791ce
Ghrysh <gispi.desu@gmail.com>
2026-03-12 15:47:39 +0700

    Fix: bug chatbot

--- app/Models/ChatbotKnowledge.php
+++ app/Models/ChatbotKnowledge.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatbotKnowledge extends Model
 {
-    //
+    protected $table = 'chatbot_knowledges';
+
+    protected $fillable = [
+        'topic',
+        'intent_name',
+        'keywords',
+        'response'
+    ];
 }
