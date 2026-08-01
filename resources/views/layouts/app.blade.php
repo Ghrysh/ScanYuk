2b15ae3834e40e6cbbff2518a287d2d9db38f723
Ghrysh <gispi.desu@gmail.com>
2026-08-01 14:03:50 +0700

    Fix blade parsing json ld @context as directive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,15 @@
     @if(isset($seoData->faq_schema) && is_array($seoData->faq_schema) && count($seoData->faq_schema) > 0)
     <script type="application/ld+json">
     {
-      "@context": "https://schema.org",
-      "@type": "FAQPage",
+      "@@context": "https://schema.org",
+      "@@type": "FAQPage",
       "mainEntity": [
         @foreach($seoData->faq_schema as $index => $faq)
         {
-          "@type": "Question",
+          "@@type": "Question",
           "name": "{{ $faq['question'] ?? '' }}",
           "acceptedAnswer": {
-            "@type": "Answer",
+            "@@type": "Answer",
             "text": "{{ $faq['answer'] ?? '' }}"
           }
         }@if(!$loop->last),@endif 
