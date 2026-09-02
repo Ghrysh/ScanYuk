d2b9a98da552681dc8d1175b2627781332ad5fbc
Ghrysh <gispi.desu@gmail.com>
2026-09-02 20:46:16 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -337,34 +337,3 @@ Route::post('/tamagotchi/{username}/login', [\App\Http\Controllers\TamagotchiWeb
 Route::get('/tamagotchi/{username}/journey/{id}', [\App\Http\Controllers\TamagotchiWebController::class, 'show'])->name('tamagotchi.journey');
 Route::post('/tamagotchi/{username}/change-username', [\App\Http\Controllers\TamagotchiWebController::class, 'changeUsername'])->name('tamagotchi.change-username');
 Route::post('/tamagotchi/{username}/reset-password', [\App\Http\Controllers\TamagotchiWebController::class, 'resetPassword'])->name('tamagotchi.reset-password');
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Models\QueueLocation;
-use App\Models\QueueService;
-use App\Models\QueueTicket;
-
-Route::get('/debug-register', function(Request $request) {
-    try {
-        $location = QueueLocation::first();
-        if (!$location) return "No location";
-        
-        $service = $location->services()->first();
-        if (!$service) return "No service";
-        
-        $queueNumber = $service->generateNextQueueNumber();
-        
-        $ticket = QueueTicket::create([
-            'queue_location_id' => $location->id,
-            'queue_service_id' => $service->id,
-            'queue_number' => $queueNumber,
-            'customer_name' => 'Debug User',
-            'customer_phone' => '08123456789',
-            'status' => 'waiting',
-            'date' => now()->toDateString(),
-        ]);
-        
-        return "SUCCESS: Ticket created with ID " . $ticket->id;
-    } catch (\Exception $e) {
-        return "ERROR: " . $e->getMessage() . "\n" . $e->getTraceAsString();
-    }
-});
